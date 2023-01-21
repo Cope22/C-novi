2cc5fb08eb57dc825fd1ed3f6bea311fbca58833
Petar Jevtic <petarjevtic02@gmail.com>
2023-01-21 21:28:33 +0100

    Lekcija 32

---
+++ Lekcija 32.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    printf ("Trenutno imam %d godina, ali sredinom godine punim %d godinu.\n",20,21);
+    printf ("Prosek ocena na fakultetu mi je %f\n", 8.2);
+    printf ("Prosek ocena na fakultetu mi je %.1f\n", 8.2);
+    return 0;
+
+}
+
+/*
+%d za ceo broj, %f za decimalni broj, %.1f (broj decimala)
