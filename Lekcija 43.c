8fbee70c594971f8cf247319b6441468c345cb2c
Petar Jevtic <petarjevtic02@gmail.com>
2023-01-28 11:09:10 +0100

    34. Milestone #1 - Solution! [Make sure to Compare with your answers

---
+++ Lekcija 43.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+
+// Write a program that prints the year you were born.
+// *Include in the printing line the subtraction calculation between the "current year" and your "age"
+
+{
+    printf ("Rodjen sam %d godine.\n", 2002);
+    printf ("%d-%d=%d\n",2002,20,2002-20);
+    return 0;
+}
