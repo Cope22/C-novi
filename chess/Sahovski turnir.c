6987d9636f9f0b767294c8f9c7e03a515748574e
Petar Jevtic <petarjevtic02@gmail.com>
2023-01-30 22:28:41 +0100

    Sahovski turnir

---
+++ chess/Sahovski turnir.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main ()
+{
+    printf ("Organizer(s) Chess club Radnicki\n");
+    printf ("Federation Serbia ( SRB )\n");
+    printf ("Tournament director: Kotevski Dragan %d\n",905968);
+    printf ("Chief Arbiter: Babic, Dragan U %d\n",928160);
+    printf ("Arbiter: NA Jevtic, Petar %d\n",9218208);
+    printf ("Time control: (Standard) %dmin/%dmoves+%dmin/end+%dsec increment per move starting from move %d\n",90,40,30,30,1);
+    printf ("Location: Belgrade\n");
+    printf ("Number of rounds: %d\n",9);
+    printf ("Tournament type: Round robin\n");
+    printf ("Rating calculation: Rating International\n");
+    printf ("Date: %d/%d/%d to %d/%d/%d\n",2023,01,25,2023,02,02);
+    printf ("Rating-Ø: %d\n",2398);
+    printf ("Pairing program: Swiss-Manager from Heinz Herzog, Swiss-Manager tournament file\n\n");
+    printf ("GM Round Robin RADNICKICHESS - Rank after Round 3\n\n");
+    printf ("IM Garifullina Leya RUS %d           %.1f\n", 2357,2.5);
+    printf ("GM Bryakin Mikhail RUS %d            %.1f\n", 2463,2.5);
+    printf ("GM Damljanovic Branko SRB %d         %d\n", 2420,2);
+    printf ("IM Grieve Harry ENG %d               %1.f\n", 2438,1.5);
+    printf ("FM Hirnese Jens GER %d               %1.f\n", 2266,1.5);
+    printf ("IM Kaasen Tor Fredrik NOR %d         %1.f\n", 2459,1.5);
+    printf ("IM Milosevic Milos SRB %d            %1.f\n", 2374,1.5);
+    printf ("GM Todorovic Goran M SRB %d          %d\n", 2346,1);
+    printf ("IM Rakotomaharo Fy Antenaina MAD %d  %d\n",2454,1);
+    printf ("Lu Miaoyi CHN %d,                    %d\n",2399,0);
+
+    return 0;
+}
