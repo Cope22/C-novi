d3a5f12533e774acd5564e503203de7b47da8f93
Petar Jevtic <petarjevtic02@gmail.com>
2023-02-05 11:28:05 +0100

    Lekcija 47

---
+++ Lekcija 47.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//int age; age = 30; double temp; temp = 26.5;
+
+int main()
+{
+	int grade1;
+	int grade2;
+	grade1 = 80;
+	grade2 = 100;
+	printf("Average = %d\n", (grade1 + grade2) / 2);
+	return 0;
+}
