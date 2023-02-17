9b01ad1398e937040e6755084ec9cdf18d298b07
Petar Jevtic <petarjevtic02@gmail.com>
2023-02-17 18:58:28 +0100

    Lekcija 89

---
+++ Lekcija 89.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a;
+    printf ("Unesi vrednost: \n");
+    scanf ("%d",&a);
+    if (a%2==0)
+        printf ("Broj je paran");
+    else
+        printf ("Broj je neparan");
+    return 0;
+
+
+
+
+
+}
