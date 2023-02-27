dc8bdbb333b5d585fdef71d66b07bffa0dbe6e7f
Petar Jevtic <petarjevtic02@gmail.com>
2023-02-27 03:39:36 +0100

    Lekcija 111

---
+++ Lekcija 111.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int mesec;
+
+    printf("Unesi broj meseca: ");
+    scanf("%d", &mesec);
+
+    switch (mesec)
+    {
+    case 1:
+        printf("Januar\n");
+        break;
+    case 2:
+        printf("Februar\n");
+        break;
+    case 3:
+        printf("Mart\n");
+        break;
+    case 4:
+        printf("April\n");
+        break;
+    case 5:
+        printf("Maj\n");
+        break;
+    case 6:
+        printf("Jun\n");
+        break;
+    case 7:
+        printf("Jul\n");
+        break;
+    case 8:
+        printf("Avgust\n");
+        break;
+    case 9:
+        printf("Septembar\n");
+        break;
+    case 10:
+        printf("Oktobar\n");
+        break;
+    case 11:
+        printf("Novembar\n");
+        break;
+    case 12:
+        printf("Decembar\n");
+        break;
+    default:
+        printf("Greska, unesi broj od 1 do 12! \n");
+        break;
+    }
+
+    return 0;
+}
