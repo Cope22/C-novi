434dddd514122c88a8c1192c7784c089c1ef745f
Petar Jevtic <petarjevtic02@gmail.com>
2023-03-17 08:59:01 +0100

    Lekcija 160

---
+++ Lekcija 160.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Napisi 1000 puta I'm sorry for not listening teacher, I will practice more next time//
+
+
+int main ()
+
+{
+    int i;
+    for (i=1;i<1000;i++)
+
+    {
+        printf ("I'm sorry for not listening teacher, I will practice more next time \n");
+    }
+    return 0;
+
+
+
+}
