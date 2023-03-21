3e69ba79c4c431088191f46bb381df769f979fe9
Petar Jevtic <petarjevtic02@gmail.com>
2023-03-21 02:42:25 +0100

    Lekcija 189

---
+++ Lekcija 189.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+void stampaj ()
+{
+
+
+printf ("Djokovic\n");
+printf ("Ima\n");
+printf ("22\n");
+}
+
+
+int main ()
+
+{
+
+    printf ("Novak\n");
+    stampaj ();
+    printf ("Grand Slam\n");
+    printf ("Titule\n");
+
+    return 0;
+
+
+}
